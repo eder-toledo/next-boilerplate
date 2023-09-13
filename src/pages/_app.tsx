7108684a10b53b569a9b8819f8e5a51f05dfaf1b
Eder Toledo <edertoledon@gmail.com>
2023-09-13 16:15:06 -0600

    Add manticore, and remove default styles.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import { MantineProvider } from "@mantine/core";
 
 import { api } from "~/utils/api";
 
@@ -12,7 +13,13 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme: "light" }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
     </SessionProvider>
   );
 };
